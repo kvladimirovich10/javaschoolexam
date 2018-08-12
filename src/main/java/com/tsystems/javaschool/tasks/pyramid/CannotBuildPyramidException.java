c831cd814ed9c5a49336e48c048f2521d8de832a
Kirill Kiselev <Kirill_Kiselev@epam.com>
2018-08-12 19:34:46 +0300

    Testing codeship

--- src/main/java/com/tsystems/javaschool/tasks/pyramid/CannotBuildPyramidException.java
+++ src/main/java/com/tsystems/javaschool/tasks/pyramid/CannotBuildPyramidException.java
@@ -3,6 +3,6 @@ package com.tsystems.javaschool.tasks.pyramid;
 public class CannotBuildPyramidException extends RuntimeException {
     @Override
     public String getMessage() {
-        return "Can not build a pyramid, try another set.";
+        return "Can not build a pyramid, try another set";
     }
 }
